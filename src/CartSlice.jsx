01a974ff2dec1b3281111fcad30a3d9466b038c0
chico530 <chico.rbshnk.530@gmail.com>
2024-11-05 10:35:46 -0500

    task 3 completed

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,28 +7,25 @@ export const CartSlice = createSlice({
     },
     reducers: {
         addItem: (state, action) => {
-            const {name, image, cost} = action.payload;
-            const existingItem = state.items.find(item => item.name === name);
+            const existingItem = state.items.find(item => item.name === action.payload.name);
             if (existingItem) {
                 existingItem.quantity++;
             } else {
-                state.items.psuh({
-                    name,
-                    image,
-                    cost,
-                    quantity: 1
+                state.items.push({
+                    ...action.payload,
+                    quantity: 1,
                 });
             }
         },
         removeItem: (state, action) => {
-            state.items = state.items.filter(item => item.name !== action.payload);
+            state.items = state.items.filter((item) => item.name !== action.payload.name);
         },
         updateQuantity: (state, action) => {
             const {name, quantity} = action.payload;
             const itemToUpdate = state.items.find(item => item.name === name);
-            if (itemToUpdate) {
+            if (itemToUpdate && quantity !==0) {
                 itemToUpdate.quantity = quantity;
-            }        
+            }
         },
     },
 });
