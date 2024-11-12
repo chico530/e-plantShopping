fe878aa2f5f36c47022fe68ebfaea8d991a46822
chico530 <chico.rbshnk.530@gmail.com>
2024-11-12 08:43:02 -0500

    job done

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -297,7 +297,7 @@ function ProductList() {
                                         <div className='product-title'>{plant.name}</div>
                                         <div className='product-description'>{plant.description}</div>
                                         <div className='product-price'>{plant.cost}</div>
-                                        <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                        <button className='product-button' onClick={() => handleAddToCart(plant)} >Add to Cart</button>
                                     </div>
                                 ))}
                             </div>
